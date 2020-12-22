945c2c3d89a7a1012cb9bf092a7af48de680709a
Alex Dzyuba <alexeydzyuba27@gmail.com>
2020-12-22 21:51:48 +0300

    Introduce basic Router

---
+++ app/framework/Router/Router.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Framework\Router;
+
+class Router {
+  public function __construct()
+  {
+    $uri = $_SERVER['REQUEST_URI'];
+
+    if ($uri === '/') {
+      require_once dirname(dirname(__DIR__)) . '/templates/pages/index.php';
+    }
+
+    if ($uri === '/some') {
+      require_once dirname(dirname(__DIR__)) . '/templates/pages/some.php';
+    }
+  }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
+use Framework\Router\Router;
+
 require dirname(__DIR__) . '/vendor/autoload.php';
+
+$router = new Router();
