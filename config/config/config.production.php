6fcaf7b156a2fd828ad89cdf68ff5a235816508f
Alex Dzyuba <alexeydzyuba27@gmail.com>
2020-12-23 21:56:00 +0300

    Delete config.dist and add production config for Heroku to work properly

---
+++ config/config/config.production.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+  'database' => [
+    'host' => 'remotemysql.com',
+    'db_name' => 'qZcAuTrJSI',
+    'username' => 'qZcAuTrJSI',
+    'password' => 'Bcfi1oY29k',
+    'charset' => 'utf8mb4',
+  ],
+];
