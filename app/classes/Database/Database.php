b55fe37ae01e67e6e937e8ae3bb662321089a055
Alex Dzyuba <alexeydzyuba27@gmail.com>
2020-12-23 16:11:10 +0300

    Add base for Database class

--- app/classes/Database/Database.php
+++ app/classes/Database/Database.php
@@ -2,7 +2,29 @@
 
 namespace App\Database;
 
+use PDO;
+
 class Database
 {
+  private PDO $manager;
 
+  public function __construct()
+  {
+    $connectionKey = "mysql:host=localhost;dbname=scandiweb_app;charset=utf8mb4";
+    $options = [
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+      PDO::ATTR_EMULATE_PREPARES => false
+    ];
+
+    $this->manager = new PDO($connectionKey, 'root', 'password', $options);
+  }
+
+  public function test() {
+    if (!$this->manager) {
+      echo 'error connecting to db';
+    }
+
+    echo 'connected to db';
+  }
 }
--- public/index.php
+++ public/index.php
@@ -1,7 +1,12 @@
 <?php
 
 use App\Router\Router;
+use App\Database\Database;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $router = new Router();
+
+$database = new Database();
+
+$database->test();
