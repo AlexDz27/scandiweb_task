9dcad946f23b37c53d34cb1db74c8e766646de71
Alex Dzyuba <alexeydzyuba27@gmail.com>
2021-01-08 21:17:25 +0300

    Remove NotFoundRoute class properties as unnecessary

--- app/classes/Router/NotFoundRoute.php
+++ app/classes/Router/NotFoundRoute.php
@@ -4,10 +4,6 @@ namespace App\Router;
 
 class NotFoundRoute extends Route
 {
-  public ?string $uri;
-  public string $controllerName;
-  public string $controllerMethod;
-
   public function __construct()
   {
     parent::__construct(null, 'NotFoundController', 'notFound');
